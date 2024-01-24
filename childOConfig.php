50a79d6d67392013d32d37e8e97a4e67df05f493
Jeann96 <janne.nurmela@hotmail.com>
2024-01-24 14:41:38 +0200

    Convert to artic observatory theme.

---
+++ childOConfig.php
@@ -0,0 +1,9 @@
+<script type="text/javascript">    
+    var childOConfig = {
+        mapExtraParams: {projection: {
+                            name: 'globe',
+                            // center: [90, 0],
+                            // parallels: [30, 30]
+        }}
+    }
+</script>
--- page-case-haltiala.php
+++
@@ -1,2 +0,0 @@
-<!--Get data from same place for all languages-->
-<?php include 'case-haltiala.php'?>
