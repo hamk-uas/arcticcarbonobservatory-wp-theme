1cdb72369bd64d4dd667866d9f366a1fd2b430bd
Jeann96 <janne.nurmela@hotmail.com>
2024-02-16 12:52:35 +0200

    Updates

--- childOConfig.php
+++ childOConfig.php
@@ -4,6 +4,10 @@
                             name: 'globe',
                             // center: [90, 0],
                             // parallels: [30, 30]
-        }}
+                            
+        }},
+        storageUrl: 'https://fmi-arctic-carbon-observatory-data.lake.fmi.fi',
+        sitesGeojsonFilename: 'sites.geojson',
+        blocksGeojsonFilename: 'blocks.geojson'
     }
 </script>
