c1f1b8e87aa720e2bc071af27b75a8bb7ed38184
Olli Niemitalo <olli4@hamk.fi>
2024-02-20 14:22:23 +0200

    Child -> ACO

--- functions.php
+++ functions.php
@@ -1,2 +1,4 @@
-<?php include 'childOConfig.php'?>
-<?php wp_enqueue_style('arcticcarbonobservatory-ACODataViewer',get_theme_file_uri("/assets/css/ACODataViewer.css"),array(),wp_get_theme()->version,'all');?>
+<?php 
+wp_enqueue_style('arcticcarbonobservatory-ACODataViewer',get_theme_file_uri("/assets/css/ACODataViewer.css"),array(),wp_get_theme()->version,'all');
+wp_enqueue_script('arcticcarbonobservatory-ACOConfigJS',get_theme_file_uri("/assets/js/ACOConfig.js"),array(),wp_get_theme()->version,true);
+?> 
